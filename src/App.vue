b3c17cb143adb26ad868675c6455d4fd20a59cfb
kangkang <976272997@qq.com>
2022-11-07 16:49:27 +0800

    购物车案例

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.app-container {
+  padding-top: 45px;
+  padding-bottom: 50px;
+}
+</style>
